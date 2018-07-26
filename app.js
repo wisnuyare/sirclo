50db9e6fb21b60202c71d699f1a3840468199938
wisnuyare <41321148+wisnuyare@users.noreply.github.com>
2018-07-27 06:14:58 +0700

    nomor3

--- app.js
+++ app.js
@@ -1,46 +1,49 @@
 new Vue({
     el: '#vue-app',
     data: {
-        nomorhp: '',
-        message: '',
-        whatsapp: '',
-        angka: 0,
-        ini1: [],
-        ini2: [],
-        arr: []
+        date: '',
+        max: '',
+        min: '',
+        variance: '',
+        average: '',
+        meanMax: 0,
+        meanMin: 0,
+        meanVar: 0,
+        tampung: []
     },
     methods: {
-        //    tambah: function() {
-        //     this.whatsapp = '<a href=https://wa.me/' + this.nomorhp + '/?text=' +this.message +'> Ini Adalah WA</a>'
-        //    },
-        //    pesan: function(){
-        //     var mess = this.message.replace(' ', '%20')
-        //     this.whatsapp = '<a href=https://wa.me/' + this.nomorhp + '/?text=' + mess +'> Ini Adalah WA</a>'
-        //    },
-        calc: function () {
-            x = this.angka;
-            for (i = 0; i < x; i++) {
-                var kedua = +this.angka - 1;
-                for (j = 0; j < 2; j++) {
-                    this.ini1.push(kedua);
-                }
-
-                var tambahan = +this.angka + 1;
-                for (j = 0; j < +this.angka; j++) {
-                    this.ini2.push(tambahan);
-                }
-                
-                console.log(this.ini2)
-                
-                const data ={
-                    hasil: this.ini1+this.ini2
-                }
-                this.arr.push(data)
-                console.log(data)
-                this.ini1 = [];
-                this.ini2 = [];
-                +this.angka --;
+        ambil: function(){
+            const data = {
+                date: this.date,
+                max: this.max,
+                min: this.min,
+                variance: this.max-this.min,
             }
+            this.tampung.push(data)
+            var total = this.tampung.length;
+            var totalmax = 0;
+            var totalmin = 0;
+            var totalvar = 0;
+            for(i = 0; i< total; i++){
+                totalmax = +this.tampung[i].max + +totalmax;
+                totalmin = +this.tampung[i].min + +totalmin;
+                totalvar = +this.tampung[i].variance + +totalvar;
+            }
+            this.meanMax = totalmax/total;
+            this.meanMin = totalmin/total;
+            this.meanVar = totalvar/total;
         },
-    }
-});
+        detail: function(e){
+            this.max = e.max;
+            localStorage.setItem("simpan", e.max);
+            this.date = e.date;
+            localStorage.setItem("simpan1", e.date);
+            this.min = e.min;
+            localStorage.setItem("simpan2", e.min);
+            this.variance = e.variance;
+            localStorage.setItem("simpan3", e.variance);
+            window.location.href = 'detail.html';
+        }
+    },
+    
+})
